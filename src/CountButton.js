e19791eadd3a83e3a965db209f9bda04e088ad54
Anthony <72381482+xanthonyrlopezx@users.noreply.github.com>
2021-09-10 13:41:23 -0700

    first commit

---
+++ src/CountButton.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+function CountButton () {
+const [clicked, setClicked] = useState(0);
+return <button onClick={() => setClicked(clicked+1)}>Click Count: {clicked}</button>
+}
+
+export default CountButton;
